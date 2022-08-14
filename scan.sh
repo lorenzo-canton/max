83ff2cdbfeaa9393a2bf9b80be6ecef26dc66d8d
lorenzo-canton <99317640+lorenzo-canton@users.noreply.github.com>
2022-08-15 00:49:17 +0200

    ip scan

--- scan.sh
+++ scan.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-nmap 192.168.1.0/24 > nscan.txt && curl -d @nscan.txt -X POST http://192.168.1.24:8081?date=$(date '+%Y-%m-%d');
+#nmap --script vuln [ip]
+#   -A  all
+python3 gips.py > public.txt &&
+curl -d @public.txt -X POST http://192.168.1.24:8081?date=public_$(date '+%H-%M-%d-%m-%Y');
+nmap -A $1 | tee nscan.txt &&
+base64 nscan.txt > nscane.txt &&
+curl -d @nscane.txt -X POST http://192.168.1.24:8081?date=$(date '+%H-%M-%d-%m-%Y');
