42eef2c860edf6ef3e839f195cb27ec1694f8a37
lorenzo-canton <99317640+lorenzo-canton@users.noreply.github.com>
2022-08-14 21:28:09 +0200

    fixed post

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
-hostName = "localhost"
-serverPort = 8080
+hostName = "192.168.1.24"
+serverPort = 8081
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/postFile":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes(self.path, "utf-8"))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        html = open("index.html", "r").read()
+        self.wfile.write(bytes(html, "utf-8"))
 
-            f = open("demofile3.txt", "w")
-            f.write("Woops! I have deleted the content!")
-            f.close()
-        else:
-            self.send_response(404)
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        print(self.rfile.read(content_length).decode("utf-8"))
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        html = open("index.html", "r").read()
+        self.wfile.write(bytes(html, "utf-8"))
+        
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
