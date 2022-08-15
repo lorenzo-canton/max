0b67fc41ba323a532188fd9b5e552a885d8ecb79
lorenzo-canton <99317640+lorenzo-canton@users.noreply.github.com>
2022-08-15 11:11:24 +0200

    decoding

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
+import base64
 
 hostName = "192.168.1.24"
 serverPort = 8081
@@ -15,10 +16,26 @@ class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
         query = urlparse(self.path).query
         params = dict(qc.split("=") for qc in query.split("&"))
-        print(params["date"])
 
         content_length = int(self.headers['Content-Length'])
-        print(self.rfile.read(content_length).decode("utf-8"))
+        content = self.rfile.read(content_length).decode("utf-8")
+
+        print(params)
+
+        try:
+            if params["enc"] == "true":
+                print(content)
+                base64_bytes = content.encode('ascii')
+                message_bytes = base64.b64decode(base64_bytes)
+                content = message_bytes.decode('ascii')
+        except:
+            pass
+        print(content)
+        
+        
+        f = open('save/%s.txt'%(params["title"]), "w")
+        f.write(content)
+        f.close()
 
         self.send_response(200)
         self.send_header("Content-type", "text/html")
