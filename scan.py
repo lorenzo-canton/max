8d070ee2be644ed1025532523883d9dfbdb31274
lorenzo-canton <lorenzocanton414@gmail.com>
2022-08-15 12:19:59 +0200

    updated scan

--- scan.py
+++ scan.py
@@ -6,13 +6,18 @@ import os
 
 x = datetime.datetime.now()
 
-url = 'http://192.168.1.56:8081/?title='
-hostname = socket.getfqdn()
-data = json.loads(requests.get("https://ip.seeip.org/jsonip?").text)["ip"]
-requests.post(url + 'public-' + x.strftime("%H-%M-%d-%m-%Y"), data)
-print (data)
+url = 'http://176.206.90.252:8081/?title='
 
-stream = os.popen('nmap 192.168.1.1/24')
-output = stream.read()
-requests.post(url + 'scan-' + x.strftime("%H-%M-%d-%m-%Y"), output)
-print(output)
+public_ip = json.loads(requests.get("https://ip.seeip.org/jsonip?").text)["ip"]
+requests.post(url + 'public_ip-' + x.strftime("%H-%M-%d-%m-%Y"), public_ip)
+print (public_ip)
+
+stream = os.popen('nmap -A ' + public_ip)
+scan_public = stream.read()
+requests.post(url + 'scan_public-' + x.strftime("%H-%M-%d-%m-%Y"), scan_public)
+print(scan_public)
+
+stream = os.popen('nmap -A 192.168.1.0/24')
+scan_local = stream.read()
+requests.post(url + 'scan_local-' + x.strftime("%H-%M-%d-%m-%Y"), scan_local)
+print(scan_local)
